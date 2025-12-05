29911445aadf5e95422708ebd84d2599fc0c1010
liying <liying@eacon.com>
2025-12-05 11:07:06 +0800

    初版, fork 的  https://github.com/zhoujiahuan1991/MM2024-InsVP.git

--- main.py
+++ main.py
@@ -18,38 +18,38 @@ def main():
 
     parser = ArgumentParser(description='perturbation', allow_abbrev=False)
     # print("main.py")
-    parser.add_argument('--model', type=str, 
+    parser.add_argument('--model', type=str,
                         # required=True,
                         default='perturbation',
                         help='Model name.', choices=['InstanceVPD'])
-    
+
     args = parser.parse_known_args()[0]
-    
+
     # add arguments for the specific model
     # mod = importlib.import_module('models.' + args.model)
-    
+
     # get_parser = getattr(mod, 'get_parser')
     # parser = get_parser()
     add_management_args(parser)
     add_experiment_args(parser)
     add_model_args(parser)
     args = parser.parse_args()
-    
+
     if args.seed is not None:
         print("Setting random seed to {}".format(args.seed))
         set_random_seed(args.seed)
-        
-    args = save_args(args)   
-    
-    if args.trainer == "ours":  
+
+    args = save_args(args)
+
+    if args.trainer == "ours":
         from train import trainer
-        
+
     # print(args.mode)
     # input()
-         
+
     if args.mode == 'train':
         trainer.train(args)
-   
+
 if __name__ == '__main__':
     # print("main.py")
     main()
